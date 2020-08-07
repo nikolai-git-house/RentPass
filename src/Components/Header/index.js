7b0d7318fea38f7d05155b2d7c2cc18c869f80ae
testuser <testuser@Kings-iMac.local>
2020-08-07 18:17:30 +0800

    update header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -24,93 +24,99 @@ class Header extends React.Component {
   render() {
     const { profile, brand, logout, history } = this.props;
     const { menuVisible } = this.state;
-    console.log(profile);
+    console.log(profile)
     return (
       <header id="page-header" className="page-header-glass">
         <div className="content-header">
-          <a className="link-fx font-w600 font-size-lg text-white" href="/">
-            <span className="smini-hidden">
-              <span className="text-white-75">
-                <img src={brand.logo} style={{ width: 150 }} alt="logo" />
+          <div className="heading">
+            <a className="link-fx font-w600 font-size-lg text-white" href="/">
+              <span className="smini-hidden">
+                <span className="text-white-75">
+                  <img src={brand.logo} style={{ width: 150 }} alt="logo" />
+                </span>
               </span>
-            </span>
-          </a>
+            </a>
 
-          <ul className={menuVisible ? "show" : "hide"}>
-            <li className="nav-main-item" onClick={this.click}>
-              <NavLink to="/profile" className={classnames("nav-main-link")}>
-                <span className="nav-main-link-name">My Profile</span>
-              </NavLink>
-            </li>
-            {/* <li className="nav-main-item">
+            <ul className={menuVisible ? "show" : "hide"}>
+              <li className="nav-main-item" onClick={this.click}>
+                <NavLink to="/profile" className={classnames("nav-main-link")}>
+                  <span className="nav-main-link-name">My Profile</span>
+                </NavLink>
+              </li>
+              {/* <li className="nav-main-item">
                 <NavLink to="/home" className={classnames("nav-main-link")}>
                   <span className="nav-main-link-name">My Home</span>
                 </NavLink>
               </li> */}
-            <li className="nav-main-item" onClick={this.click}>
-              <NavLink to="/housemates" className={classnames("nav-main-link")}>
-                <span className="nav-main-link-name">My Housemates</span>
-              </NavLink>
-            </li>
-            <li className="nav-main-item" onClick={this.click}>
-              <NavLink
-                to="/newproperty"
-                className={classnames("nav-main-link")}
-              >
-                <span className="nav-main-link-name">My Properties</span>
-              </NavLink>
-            </li>
-            <li className="nav-main-item" onClick={this.click}>
-              <NavLink
-                to="/referencing"
-                className={classnames("nav-main-link")}
-              >
-                <span className="nav-main-link-name">Referencing</span>
-              </NavLink>
-            </li>
-            <li className="nav-main-item" onClick={this.click}>
-              <NavLink to="/concierge" className={classnames("nav-main-link")}>
-                <span className="nav-main-link-name">Concierge</span>
-              </NavLink>
-            </li>
-            <li className="nav-main-item" onClick={this.click}>
-              <NavLink to="/tickets" className={classnames("nav-main-link")}>
-                <span className="nav-main-link-name">Tickets</span>
-              </NavLink>
-            </li>
-            <li className="nav-main-item" onClick={this.click}>
-              <NavLink to="/shop" className={classnames("nav-main-link")}>
-                <span className="nav-main-link-name">Shop</span>
-              </NavLink>
-            </li>
-          </ul>
-          <div>
-            <div className="dropdown">
-              <button
-                type="button"
-                className="btn nav-btn top-btn"
-                id="page-header-notifications-dropdown"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <i className="fas fa-user" />
-                <span className="badge  badge-pill">
-                  {profile && profile.firstname}
-                </span>
-              </button>
-              <div
-                className="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"
-                aria-labelledby="page-header-notifications-dropdown"
-              >
-                <div className="p-2 border-top">
-                  <button
-                    className="btn btn-light btn-block text-center"
-                    onClick={logout}
-                  >
-                    <i className="nav-main-link-icon si si-logout" />
-                    &nbsp;&nbsp;&nbsp;Sign Out
-                  </button>
+              <li className="nav-main-item" onClick={this.click}>
+                <NavLink
+                  to="/housemates"
+                  className={classnames("nav-main-link")}
+                >
+                  <span className="nav-main-link-name">My Housemates</span>
+                </NavLink>
+              </li>
+              <li className="nav-main-item" onClick={this.click}>
+                <NavLink
+                  to="/newproperty"
+                  className={classnames("nav-main-link")}
+                >
+                  <span className="nav-main-link-name">My Properties</span>
+                </NavLink>
+              </li>
+              <li className="nav-main-item" onClick={this.click}>
+                <NavLink
+                  to="/referencing"
+                  className={classnames("nav-main-link")}
+                >
+                  <span className="nav-main-link-name">Referencing</span>
+                </NavLink>
+              </li>
+              <li className="nav-main-item" onClick={this.click}>
+                <NavLink
+                  to="/concierge"
+                  className={classnames("nav-main-link")}
+                >
+                  <span className="nav-main-link-name">Concierge</span>
+                </NavLink>
+              </li>
+              <li className="nav-main-item" onClick={this.click}>
+                <NavLink to="/tickets" className={classnames("nav-main-link")}>
+                  <span className="nav-main-link-name">Tickets</span>
+                </NavLink>
+              </li>
+              <li className="nav-main-item" onClick={this.click}>
+                <NavLink to="/shop" className={classnames("nav-main-link")}>
+                  <span className="nav-main-link-name">Shop</span>
+                </NavLink>
+              </li>
+            </ul>
+            <div>
+              <div className="dropdown">
+                <button
+                  type="button"
+                  className="btn nav-btn top-btn"
+                  id="page-header-notifications-dropdown"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <i className="fas fa-user" />
+                  <span className="badge  badge-pill">{profile && profile.firstname}</span>
+                </button>
+                <div
+                  className="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"
+                  aria-labelledby="page-header-notifications-dropdown"
+                >
+                  <div className="p-2 border-top">
+                    <button
+                      className="btn btn-light btn-block text-center"
+                      onClick={logout}
+                    >
+                      <i className="nav-main-link-icon si si-logout" />
+                      &nbsp;&nbsp;&nbsp;Sign Out
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>

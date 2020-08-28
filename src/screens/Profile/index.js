636900abff280dd6e300c327d4d702f5eac7f438
victor.belikhin <victor.belikhin@gmail.com>
2020-08-28 17:48:50 +0300

    rent pass profile

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -16,6 +16,8 @@ const pending_img = require("../../assets/media/icons/pending.png");
 const phone_img = require("../../images/profile/phone.png");
 const heart_img = require("../../images/profile/heart.png");
 const profession_img = require("../../images/profile/profession.png");
+const firstname_img = require("../../images/profile/firstname.png");
+const lastname_img = require("../../images/profile/secondname.png");
 const superhero_img = require("../../images/profile/superhero.png");
 const salary_img = require("../../images/profile/growth.png");
 const UKIcon = require("../../images/profile/uk.png");
@@ -23,6 +25,13 @@ const USAIcon = require("../../images/profile/usa.png");
 const IrelandIcon = require("../../images/profile/ireland.png");
 const CanadaIcon = require("../../images/profile/canada.png");
 const ApproveIcon = require("../../images/profile/done.png");
+
+const identity_img = require("../../images/profile/identity.png");
+const credit_img = require("../../images/profile/credit.png");
+const final_img = require("../../images/profile/final_decision.png");
+const rental_img = require("../../images/profile/rental.png");
+const employment_img = require("../../images/profile/employment.png");
+const pause_img = require("../../images/profile/pause.png");
 
 const Styles = {
   option: (provided, state) => ({
@@ -146,7 +155,7 @@ class Profile extends React.Component {
           },
           {
             canvas: true,
-            orientation: orientation,
+            meta: true,
           }
         );
       });
@@ -158,6 +167,7 @@ class Profile extends React.Component {
     let {
       avatar_url,
       firstname,
+      surname,
       phonenumber,
       dob,
       profession,
@@ -223,12 +233,41 @@ class Profile extends React.Component {
                   <img src={ApproveIcon} alt="active" />
                   <img src={TerritoryIcon[territory]} alt="flag" />
                 </div>
-                <p>
-                  <b>{firstname}</b>
-                </p>
               </div>
 
               <div className="personal-view">
+                <div className="row_item">
+                  <img src={firstname_img} width="30" alt="firstname"></img>
+                  <p>{firstname}</p>
+                </div>
+                <div className="row_item">
+                  <img src={lastname_img} width="30" alt="surname"></img>
+                  <p className="row_item_value">
+                    {editfield === "surname" ? (
+                      <input
+                        className="input_box"
+                        value={editValue}
+                        onChange={(e) =>
+                          this.setState({ editValue: e.target.value })
+                        }
+                      ></input>
+                    ) : (
+                      surname || "Surname"
+                    )}
+                  </p>
+                  <div
+                    className="edit"
+                    onClick={() => this.onChangeProfileData("surname")}
+                  >
+                    <i
+                      className={
+                        editfield === "surname"
+                          ? "si si-check"
+                          : "si si-settings"
+                      }
+                    />
+                  </div>
+                </div>
                 <div className="row_item">
                   <img src={phone_img} width="30" alt="phonenumber"></img>
                   <p>{phonenumber}</p>
@@ -246,7 +285,7 @@ class Profile extends React.Component {
                         }
                       ></input>
                     ) : (
-                      `Age: ${_calculateAge(dob)}`
+                      `Age: ${_calculateAge(dob) || ""}`
                     )}
                   </p>
                   <div
@@ -260,7 +299,7 @@ class Profile extends React.Component {
                     />
                   </div>
                 </div>
-                <div className="row_item">
+                {/* <div className="row_item">
                   <img src={profession_img} width="30" alt="profession"></img>
                   <p className="row_item_value">
                     {editfield === "profession" ? (
@@ -358,7 +397,7 @@ class Profile extends React.Component {
                       }
                     />
                   </div>
-                </div>
+                </div> */}
                 <input
                   type="file"
                   id="example-file-input"
@@ -371,11 +410,110 @@ class Profile extends React.Component {
               </div>
             </div>
           </div>
-          <div className="col-md-5 col-lg-5 col-xl-5" style={{ padding: 15 }}>
+          <div className="col-md-7 col-lg-7 col-xl-7" style={{ padding: 15 }}>
             <div className="content">
               <div className="block-fx-pop">
                 <div className="renting-view">
-                  <div className="row_item">
+                  <div className="row_editable_item">
+                    <img src={profession_img} width="30" alt="profession"></img>
+                    <p className="row_item_value">
+                      {editfield === "profession" ? (
+                        <div style={{ flex: 1 }}>
+                          <ReactSelect
+                            value={{ label: editValue, value: editValue }}
+                            onChange={(e) => {
+                              this.setState({ editValue: e.value });
+                            }}
+                            options={professions.map((obj) => ({
+                              value: obj,
+                              label: obj,
+                            }))}
+                            isSearchable={false}
+                            styles={Styles}
+                          ></ReactSelect>
+                        </div>
+                      ) : (
+                        profession || "Profession"
+                      )}
+                    </p>
+                    <div
+                      className="edit"
+                      onClick={() => this.onChangeProfileData("profession")}
+                    >
+                      <i
+                        className={
+                          editfield === "profession"
+                            ? "si si-check"
+                            : "si si-settings"
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div className="row_editable_item">
+                    <img src={salary_img} width="30" alt="salary"></img>
+                    <p className="row_item_value">
+                      £{" "}
+                      {editfield === "salary" ? (
+                        <input
+                          className="input_box"
+                          value={editValue}
+                          onChange={(e) =>
+                            this.setState({ editValue: e.target.value })
+                          }
+                        ></input>
+                      ) : (
+                        salary || "Monthly Salary"
+                      )}
+                    </p>
+                    <div
+                      className="edit"
+                      onClick={() => this.onChangeProfileData("salary")}
+                    >
+                      <i
+                        className={
+                          editfield === "salary"
+                            ? "si si-check"
+                            : "si si-settings"
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div className="row_editable_item">
+                    <img src={identity_img} width="30" alt="salary"></img>
+                    <p className="row_item_value">Identity & right to rent</p>
+                    <div className="edit">
+                      <img src={pause_img} alt="pause" />
+                    </div>
+                  </div>
+                  <div className="row_editable_item">
+                    <img src={credit_img} width="30" alt="salary"></img>
+                    <p className="row_item_value">Credit</p>
+                    <div className="edit">
+                      <img src={pause_img} alt="pause" />
+                    </div>
+                  </div>
+                  <div className="row_editable_item">
+                    <img src={employment_img} width="30" alt="salary"></img>
+                    <p className="row_item_value">Employment, affordability & income</p>
+                    <div className="edit">
+                      <img src={pause_img} alt="pause" />
+                    </div>
+                  </div>
+                  <div className="row_editable_item">
+                    <img src={rental_img} width="30" alt="salary"></img>
+                    <p className="row_item_value">Rental history</p>
+                    <div className="edit">
+                      <img src={pause_img} alt="pause" />
+                    </div>
+                  </div>
+                  <div className="row_editable_item">
+                    <img src={final_img} width="30" alt="salary"></img>
+                    <p className="row_item_value">Overall decision</p>
+                    <div className="edit">
+                      <img src={pause_img} alt="pause" />
+                    </div>
+                  </div>
+                  {/* <div className="row_item">
                     <p>KYC</p>
                     <img src={pending_img} width="30" alt="cross"></img>
                   </div>
@@ -402,7 +540,7 @@ class Profile extends React.Component {
                   <div className="row_item">
                     <p>Rental history</p>
                     <img src={pending_img} width="30" alt="cross"></img>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
